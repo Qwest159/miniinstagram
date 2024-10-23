0de4e0d17009e1824a4a4a571389085c1fa331df
Qwest159 <duchesne-raphael@hotmail.com>
2024-10-23 12:50:18 +0200

    img

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,9 +1,10 @@
 <div>
     <a class="flex flex-col h-full space-y-4 bg-white rounded-md shadow-md p-5 w-full hover:shadow-lg hover:scale-105 transition"
     href="{{ route('front.posts.show', $post) }}">
-    {{-- <div class="uppercase font-bold text-gray-800">
-        {{ $article->title }}
-    </div> --}}
+    <div class="uppercase font-bold text-gray-800">
+        {{ $post->img }}
+    </div>
+    <img src="{{ Storage::url($post->img_path) }}" alt="illustration du post">
     <div class="flex-grow text-gray-700 text-sm text-justify">
         {{ Str::limit($post->body, 120) }}
     </div>
